ef6002928f0bca1f2cdec91dd314d4f1b6b68518
Mark Gardner <mark.gardner@microsoft.com>
2018-11-19 11:34:51 -0500

    adding queue trigger

--- function/FunctionApp/Function2.cs
+++ function/FunctionApp/Function2.cs
@@ -6,9 +6,12 @@ namespace FunctionApp
     public static class Function2
     {
         [FunctionName("Function2")]
-        public static void Run([ServiceBusTrigger("functiontop","functionsub",Connection = "AzureWebJobsServiceBus")]string mySbMsg, ILogger log)
+        [return: Queue("functionqueue")]
+
+        public static string Run([ServiceBusTrigger("functiontop","functionsub",Connection = "AzureWebJobsServiceBus")]string mySbMsg, ILogger log)
         {
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            return mySbMsg;
         }
     }
 }
